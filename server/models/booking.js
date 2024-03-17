243b7ef77ac02cfdbac5aeda87fd096b9b2c9bb8
SuminChhetri <144243748+SuminChhetri@users.noreply.github.com>
2024-03-17 11:38:19 -0400

    schema to store bookings

---
+++ server/models/booking.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  student: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Student',
+    required: true
+  },
+  tutor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tutor',
+    required: true
+  },
+  sessionDate: {
+    type: Date,
+    required: true
+  },
+  sessionLength: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['confirmed', 'cancelled'],
+    default: 'confirmed'
+  }
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
