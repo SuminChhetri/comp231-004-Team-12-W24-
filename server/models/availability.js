52db426f87a235a71cab868a32575e251f260469
SuminChhetri <144243748+SuminChhetri@users.noreply.github.com>
2024-04-01 10:13:31 -0400

    updated days field

--- server/models/availability.js
+++ server/models/availability.js
@@ -1,26 +1,25 @@
-// models/availability.js
 const mongoose = require('mongoose');
 
 const availabilitySchema = new mongoose.Schema({
-    tutor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Tutor',
-        required: true
-    },
-    dayOfWeek: {
-        type: String,
-        required: true
-    },
-    startTime: {
-        type: String,
-        required: true
-    },
-    endTime: {
-        type: String,
-        required: true
-    }
+  tutor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tutor',
+    required: true,
+    unique: true // Ensures each tutor has only one availability
+  },
+  days: {
+    type: [String],
+    required: true,
+    enum: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  },
+  startTime: {
+    type: Date,
+    required: true
+  },
+  endTime: {
+    type: Date,
+    required: true
+  }
 });
 
-const Availability = mongoose.model('Availability', availabilitySchema);
-
-module.exports = Availability;
+module.exports = mongoose.model('Availability', availabilitySchema);
