056822a32b3802509b4ae5c905d4d242f80544e8
SuminChhetri <144243748+SuminChhetri@users.noreply.github.com>
2024-04-07 00:42:18 -0400

    updates with manage college, tutor, studnet and booking controller for admin

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -61,6 +61,7 @@ exports.adminRegister = async (req, res) => {
             email,
             password: hashedPassword
         });
+        console.log('New Admin:', newAdmin);
 
         // Save the admin to the database
         await newAdmin.save();
@@ -68,5 +69,236 @@ exports.adminRegister = async (req, res) => {
         res.status(201).json({ message: 'Admin registration successful' });
     } catch (error) {
         res.status(400).json({ message: error.message });
+    }
+};
+
+
+exports.getAllBookings = async (req, res) => {
+    try {
+        // Fetch all bookings with populated student and tutor fields
+        const bookings = await Booking.find()
+            .populate('student', 'firstName lastName') // Populate firstName and lastName of the student
+            .populate('tutor', 'firstName lastName'); // Populate firstName and lastName of the tutor
+
+        res.status(200).json({ bookings });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.deleteBooking = async (req, res) => {
+    try {
+        const { bookingId } = req.params;
+
+        // Find the booking
+        const booking = await Booking.findById(bookingId);
+
+        if (!booking) {
+            return res.status(404).json({ message: 'Booking not found' });
+        }
+
+        // Remove the booking from the student's bookings
+        await Student.updateOne(
+            { _id: booking.student },
+            { $pull: { bookings: bookingId } }
+        );
+
+        // Remove the booking from the tutor's bookings
+        await Tutor.updateOne(
+            { _id: booking.tutor },
+            { $pull: { bookings: bookingId } }
+        );
+
+        // Delete the booking
+        await Booking.findByIdAndDelete(bookingId);
+
+        res.status(200).json({ message: 'Booking deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+
+  
+  // Function to get all students
+  exports.getAllStudents = async (req, res) => {
+    try {
+        const students = await Student.find().populate('college', 'collegeName'); // Populate college field with name
+        res.status(200).json({ students });
+    } catch (error) {
+      console.error('Error fetching students:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  };
+  
+  // Function to update student profile
+  exports.updateStudent = async (req, res) => {
+    try {
+      const studentId = req.params.studentId;
+      const { firstName, lastName, email, phoneNumber} = req.body;
+      const updatedFields = {
+        firstName,
+        lastName,
+        email,
+        phoneNumber
+      };
+      const updatedStudent = await Student.findByIdAndUpdate(studentId, updatedFields, { new: true });
+      if (!updatedStudent) {
+        return res.status(404).json({ message: 'Student not found' });
+      }
+      res.status(200).json({ message: 'Student profile updated successfully', student: updatedStudent });
+    } catch (error) {
+      console.error('Error editing student profile:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  };
+  
+  // Function to delete student profile
+  exports.deleteStudent = async (req, res) => {
+    try {
+      const studentId = req.params.studentId;
+      await Student.findByIdAndDelete(studentId);
+      res.status(200).json({ message: 'Student profile deleted successfully' });
+    } catch (error) {
+      console.error('Error deleting student profile:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  };
+
+// Function to get details of a specific student by ID
+exports.getStudentById = async (req, res) => {
+  try {
+    const studentId = req.params.studentId;
+    const student = await Student.findById(studentId);
+    if (!student) {
+      return res.status(404).json({ message: 'Student not found' });
+    }
+    res.status(200).json({ student });
+  } catch (error) {y
+    console.error('Error fetching student details:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+// Function to get all tutors
+exports.getAllTutors = async (req, res) => {
+    try {
+        const tutors = await Tutor.find().populate('college', 'collegeName');
+        res.status(200).json({ tutors });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Function to update tutor profile
+exports.updateTutor = async (req, res) => {
+    try {
+        const tutorId = req.params.tutorId;
+        const { firstName, lastName, email, phoneNumber } = req.body;
+        
+        // Check if the tutor exists
+        const existingTutor = await Tutor.findById(tutorId);
+        if (!existingTutor) {
+            return res.status(404).json({ message: 'Tutor not found' });
+        }
+
+        // Update tutor fields
+        existingTutor.firstName = firstName;
+        existingTutor.lastName = lastName;
+        existingTutor.email = email;
+        existingTutor.phoneNumber = phoneNumber;
+
+        // Save the updated tutor
+        const updatedTutor = await existingTutor.save();
+
+        res.status(200).json({ message: 'Tutor profile updated successfully', tutor: updatedTutor });
+    } catch (error) {
+        console.error('Error updating tutor profile:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+// Function to delete tutor profile
+exports.deleteTutor = async (req, res) => {
+    try {
+        const tutorId = req.params.tutorId;
+        await Tutor.findByIdAndDelete(tutorId);
+        res.status(200).json({ message: 'Tutor profile deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting tutor profile:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+// Function to get details of a specific tutor by ID
+exports.getTutorById = async (req, res) => {
+    try {
+        const tutorId = req.params.tutorId;
+        const tutor = await Tutor.findById(tutorId);
+        if (!tutor) {
+            return res.status(404).json({ message: 'Tutor not found' });
+        }
+        res.status(200).json({ tutor });
+    } catch (error) {
+        console.error('Error fetching tutor details:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+//Function to update details of a college
+exports.updateCollege = async (req, res) => {
+    try {
+        const { collegeId } = req.params;
+        const { collegeName, location, email, phoneNumber, description, picture } = req.body;
+
+        // Find the college by ID and update details
+        const updatedCollege = await College.findByIdAndUpdate(collegeId, {
+            collegeName,
+            location,
+            email,
+            phoneNumber,
+            description,
+            picture
+        }, { new: true });
+
+        if (!updatedCollege) {
+            return res.status(404).json({ message: 'College not found' });
+        }
+
+        res.status(200).json({ message: 'College details updated successfully', college: updatedCollege });
+    } catch (error) {
+        console.error('Error updating college:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+// Function to delete a college
+exports.deleteCollege = async (req, res) => {
+    try {
+        const { collegeId } = req.params;
+
+        // Find the college by ID and delete
+        const deletedCollege = await College.findByIdAndDelete(collegeId);
+
+        if (!deletedCollege) {
+            return res.status(404).json({ message: 'College not found' });
+        }
+
+        res.status(200).json({ message: 'College deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting college:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+// Function to retrieve all colleges
+exports.getAllColleges = async (req, res) => {
+    try {
+        const colleges = await College.find();
+        res.status(200).json({ colleges });
+    } catch (error) {
+        console.error('Error fetching colleges:', error);
+        res.status(500).json({ message: 'Internal server error' });
     }
 };
