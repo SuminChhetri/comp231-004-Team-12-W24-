c340258b2d58158c51c0112843ec61f646575fea
SuminChhetri <144243748+SuminChhetri@users.noreply.github.com>
2024-04-07 00:51:29 -0400

    rotes to handle manage student, tutor, college and bookings

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -5,3 +5,23 @@ const { authenticateAdmin } = require('../middlewares/authenticateJWT.js');
 
 router.post('/login', adminController.adminLogin);
 router.post('/register', adminController.adminRegister);
+
+router.delete('/bookings/:bookingId',authenticateAdmin, adminController.deleteBooking);
+router.get('/bookings', authenticateAdmin,adminController.getAllBookings);
+
+router.get('/students', authenticateAdmin, adminController.getAllStudents);
+router.put('/students/:studentId', authenticateAdmin, adminController.updateStudent);
+router.delete('/students/:studentId', authenticateAdmin, adminController.deleteStudent);
+router.get('/students/:studentId', authenticateAdmin, adminController.getStudentById);
+
+router.get('/tutors', authenticateAdmin, adminController.getAllTutors);
+router.put('/tutors/:tutorId', authenticateAdmin, adminController.updateTutor);
+router.delete('/tutors/:tutorId', authenticateAdmin, adminController.deleteTutor);
+router.get('/tutors/:tutorId', authenticateAdmin, adminController.getTutorById);
+
+
+router.put('/colleges/:collegeId', authenticateAdmin, adminController.updateCollege);
+router.delete('/colleges/:collegeId', authenticateAdmin, adminController.deleteCollege);
+router.get('/colleges', authenticateAdmin, adminController.getAllColleges);
+
+module.exports = router;
